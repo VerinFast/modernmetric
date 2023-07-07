492ea824b91456a4387a47d81e62267752806b3e
Jason Nichols <jason@startupos.dev>
2023-07-07 14:20:45 -0400

    version bump

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="modernmetric",
-    version="1.0.0",
+    version="1.0.2",
     author="Jason Nichols",
     author_email="jason@startupos.dev",
     description="Calculate code metrics in various languages",
