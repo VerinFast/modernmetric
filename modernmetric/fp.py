23dc96f430dc2d0ea6b80d00be5017b417e13533
Sean T. Conrad <seantconrad@gmail.com>
2025-03-07 12:36:15 -0500

    fixes

--- modernmetric/fp.py
+++ modernmetric/fp.py
@@ -52,9 +52,8 @@ def file_process(_file, _args, _importer, cache: Optional[Cache] = None):
     try:
         with open(_file, "rb") as i:
             _cnt = i.read()
-            _enc = chardet.detect(_cnt)
-            _file_encoding = _enc.get("encoding") or "utf-8"  # fallback if None
-            _cnt = _cnt.decode(_file_encoding, errors="replace").encode("utf-8")
+            _enc = chardet.detect(_cnt) or "utf-8"
+            _cnt = _cnt.decode(_enc, errors="replace").encode("utf-8")
 
         _localImporter = {k: FilteredImporter(v, _file) for k, v in _importer.items()}
         tokens = list(_lexer.get_tokens(_cnt))
